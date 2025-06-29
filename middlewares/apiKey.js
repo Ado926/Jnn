44b630586a759fa9f93d13df2b4a757583814e71
Ado <adonaylester85@gmail.com>
2025-06-28 21:23:03 -0600

    Delete middlewares directory

--- middlewares/apiKey.js
+++
@@ -1,5 +0,0 @@
-export default function (req, res, next) {
-  const key = req.query.apikey
-  if (!key || key !== 'adonix') return res.status(403).json({ error: 'Invalid or missing API key' })
-  next()
-}
