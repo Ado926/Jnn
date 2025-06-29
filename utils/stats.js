2d5938c644a497570fe49b2eb6db2d935b9afd08
Ado <adonaylester85@gmail.com>
2025-06-28 21:23:13 -0600

    Delete utils directory

--- utils/stats.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs-extra'
-
-const file = './database.json'
-
-export function getStats() {
-  if (!fs.existsSync(file)) return { total: 0, endpoints: {} }
-  const db = fs.readJsonSync(file)
-  return db.stats || { total: 0, endpoints: {} }
-}
